343814ac013e3465500f0f52bc905f031b58ee15
pyh20 <counq52@163.com>
2026-05-26 22:04:04 +0800

    feat(wms): 添加商品导入监听器和库存管理功能
    - 新增ImportGoodsListener实现Excel数据导入监听功能
    - 创建WmsInventory实体类和相关服务接口及实现
    - 添加库存变更表WmsInventoryTrans及相关业务逻辑
    - 实现库存分配、发货、拣货、上架等库存变更操作
    - 提供库存按唯一键查询功能和批量导入导出支持

---
+++ jeecg-module-wms/src/main/java/org/jeecg/modules/wms/inventory/service/impl/WmsInventoryServiceImpl.java
@@ -0,0 +1,31 @@
+package org.jeecg.modules.wms.inventory.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.apache.commons.lang3.StringUtils;
+import org.jeecg.modules.wms.inventory.entity.WmsInventory;
+import org.jeecg.modules.wms.inventory.mapper.WmsInventoryMapper;
+import org.jeecg.modules.wms.inventory.service.IWmsInventoryService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: 库存表
+ * @Author: jeecg-boot
+ * @Date:   2025-11-26
+ * @Version: V1.0
+ */
+@Service
+public class WmsInventoryServiceImpl extends ServiceImpl<WmsInventoryMapper, WmsInventory> implements IWmsInventoryService {
+
+    @Override
+    public WmsInventory getInventoryByUniqueKey(String productId, String locationCode, String batchNumber) {
+        //库存唯一：商品id、储位编码、批号
+        LambdaQueryWrapper<WmsInventory> eq = new LambdaQueryWrapper<WmsInventory>()
+                .eq(WmsInventory::getProductId, productId)
+                .eq(WmsInventory::getLocationCode, locationCode)
+                .eq(StringUtils.isNotEmpty(batchNumber), WmsInventory::getBatchNumber, batchNumber)
+                .eq(StringUtils.isEmpty(batchNumber), WmsInventory::getBatchNumber, "");
+        return this.getOne(eq);
+    }
+}
